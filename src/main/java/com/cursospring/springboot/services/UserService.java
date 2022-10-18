f9c58447c820bc55352d65b81a6083b3254e8e13
iurymagano <iurymagano17@live.com>
2022-10-17 21:40:37 -0300

    componente registro

---
+++ src/main/java/com/cursospring/springboot/services/UserService.java
@@ -0,0 +1,28 @@
+package com.cursospring.springboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursospring.springboot.entities.User;
+import com.cursospring.springboot.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
